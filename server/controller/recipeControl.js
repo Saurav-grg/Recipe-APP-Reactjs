ce9341b3078f85cfd0aa1ed004c2292ae0280d1f
Saurav-grg <menemy2323@gmail.com>
2024-09-11 17:14:44 +0545

    removed logs

--- server/controller/recipeControl.js
+++ server/controller/recipeControl.js
@@ -5,8 +5,7 @@ const getRecipes = async (req, res) => {
   try {
     const { query } = req.params;
     const url = `https://api.edamam.com/api/recipes/v2?type=public&${query}&app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}`;
-    //&random=true&field=externalId&field=uri&field=image&field=label&field=images&field=calories&field=totalTime
-    console.log(url);
+
     const response = await axios.get(url);
     if (response.status === 200) {
       const data = response.data;
@@ -25,7 +24,6 @@ const getRecipe = async (req, res) => {
   try {
     const { id } = req.params;
     const url = `https://api.edamam.com/api/recipes/v2/${id}?type=public&app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}`;
-    console.log(url);
     const response = await axios.get(url);
     if (response.status === 200) {
       const data = response.data;
--- server/server.js
+++ server/server.js
@@ -1,26 +1,14 @@
 const express = require('express');
 const app = express();
-// const cors = require('cors');
 const path = require('path');
 require('dotenv').config();
 
 const recipeRoutes = require('./routes/recipeRoutes');
 app.use(express.json());
-// const corsOptions = {
-//   origin: 'http://localhost:5173',
-//   credentials: true,
-// };
-// app.use(cors(corsOptions));
 
-app.use((req, res, next) => {
-  console.log(req.path, req.method);
-  next();
-});
-// const __dirname = path.resolve();
 app.use('/api/recipes', recipeRoutes);
 if (process.env.NODE_ENV === 'production') {
   console.log('server started on production');
-  // console.log(__dirname);
   const rootDir = path.resolve(__dirname, '..');
   app.use(express.static(path.join(rootDir, '/client/dist')));
   app.get('*', (req, res) => {
